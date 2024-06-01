2429e638529d0988e0aabbb1ee586fe0a0dd68c9
Rafae1Menezes <menezes.inbox@gmail.com>
2024-06-01 10:18:43 -0300

    feat: migrations

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var db = new Data.AplicationContext();
+            // db.Database.Migrate(); // não recomendado
+
+            var existe = db.Database.GetPendingMigrations().Any();
+            if(existe)
+            {
+                Console.WriteLine("Existe migrações pendentes");
+            }
+        }
+    }
+}
